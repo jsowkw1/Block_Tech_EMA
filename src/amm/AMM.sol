b456c7312083a54a84b77d014443671c937143ab
jsowkw1 <myhsagbaj@gmail.com>
2026-05-15 13:52:58 +0500

    feat(amm): implement swap functionality

--- src/amm/AMM.sol
+++ src/amm/AMM.sol
@@ -22,12 +22,7 @@ contract AMM {
     error InvalidAmount();
     error InsufficientLiquidity();
 
-    event LiquidityAdded(
-        address indexed user,
-        uint256 amount0,
-        uint256 amount1,
-        uint256 liquidity
-    );
+    event LiquidityAdded(address indexed user, uint256 amount0, uint256 amount1, uint256 liquidity);
 
     constructor(address _token0, address _token1) {
         token0 = IERC20(_token0);
@@ -36,10 +31,7 @@ contract AMM {
         lpToken = new LPToken();
     }
 
-    function addLiquidity(
-        uint256 amount0,
-        uint256 amount1
-    ) external returns (uint256 liquidity) {
+    function addLiquidity(uint256 amount0, uint256 amount1) external returns (uint256 liquidity) {
         if (amount0 == 0 || amount1 == 0) {
             revert InvalidAmount();
         }
@@ -54,36 +46,77 @@ contract AMM {
         reserve0 += amount0;
         reserve1 += amount1;
 
-        emit LiquidityAdded(
-            msg.sender,
-            amount0,
-            amount1,
-            liquidity
-        );
-    }
-    
-    function removeLiquidity(uint256 liquidity) 
-    external returns (uint256 amount0, uint256 amount1) {
-
-    if (liquidity == 0) {
-        revert InvalidAmount();
+        emit LiquidityAdded(msg.sender, amount0, amount1, liquidity);
     }
 
-    uint256 totalSupply = lpToken.totalSupply();
+    function removeLiquidity(uint256 liquidity) external returns (uint256 amount0, uint256 amount1) {
+        if (liquidity == 0) {
+            revert InvalidAmount();
+        }
 
-    amount0 = (liquidity * reserve0) / totalSupply;
-    amount1 = (liquidity * reserve1) / totalSupply;
+        uint256 totalSupply = lpToken.totalSupply();
 
-    if (amount0 == 0 || amount1 == 0) {
-        revert InsufficientLiquidity();
+        amount0 = (liquidity * reserve0) / totalSupply;
+        amount1 = (liquidity * reserve1) / totalSupply;
+
+        if (amount0 == 0 || amount1 == 0) {
+            revert InsufficientLiquidity();
+        }
+
+        lpToken.burn(msg.sender, liquidity);
+
+        reserve0 -= amount0;
+        reserve1 -= amount1;
+
+        token0.safeTransfer(msg.sender, amount0);
+        token1.safeTransfer(msg.sender, amount1);
     }
 
-    lpToken.burn(msg.sender, liquidity);
+    function getAmountOut(uint256 amountIn, uint256 reserveIn, uint256 reserveOut)
+        public
+        pure
+        returns (uint256 amountOut)
+    {
+        if (amountIn == 0) revert InvalidAmount();
+        if (reserveIn == 0 || reserveOut == 0) {
+            revert InsufficientLiquidity();
+        }
 
-    reserve0 -= amount0;
-    reserve1 -= amount1;
+        uint256 amountInWithFee = amountIn * 997;
 
-    token0.safeTransfer(msg.sender, amount0);
-    token1.safeTransfer(msg.sender, amount1);
+        uint256 numerator = amountInWithFee * reserveOut;
+
+        uint256 denominator = (reserveIn * 1000) + amountInWithFee;
+
+        amountOut = numerator / denominator;
+    }
+
+    function swap(address tokenIn, uint256 amountIn) external returns (uint256 amountOut) {
+        if (amountIn == 0) {
+            revert InvalidAmount();
+        }
+
+        bool isToken0 = tokenIn == address(token0);
+
+        if (tokenIn != address(token0) && tokenIn != address(token1)) {
+            revert InvalidAmount();
+        }
+
+        (IERC20 inputToken, IERC20 outputToken, uint256 reserveIn, uint256 reserveOut) =
+            isToken0 ? (token0, token1, reserve0, reserve1) : (token1, token0, reserve1, reserve0);
+
+        inputToken.safeTransferFrom(msg.sender, address(this), amountIn);
+
+        amountOut = getAmountOut(amountIn, reserveIn, reserveOut);
+
+        outputToken.safeTransfer(msg.sender, amountOut);
+
+        if (isToken0) {
+            reserve0 += amountIn;
+            reserve1 -= amountOut;
+        } else {
+            reserve1 += amountIn;
+            reserve0 -= amountOut;
+        }
     }
 }
