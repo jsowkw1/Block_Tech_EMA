4dbf3dcc9bc7836d05c6c71f911ac5b0328d6999
jsowkw1 <myhsagbaj@gmail.com>
2026-05-15 13:59:39 +0500

    implement advanced amm logic with fmt

--- src/amm/AMM.sol
+++ src/amm/AMM.sol
@@ -5,24 +5,32 @@ import "./LPToken.sol";
 
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import "openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-contract AMM {
+contract AMM is ReentrancyGuard {
     using SafeERC20 for IERC20;
 
-    IERC20 public token0;
-    IERC20 public token1;
+    IERC20 public immutable token0;
+    IERC20 public immutable token1;
 
-    LPToken public lpToken;
+    LPToken public immutable lpToken;
 
     uint256 public reserve0;
     uint256 public reserve1;
 
-    uint256 public constant FEE = 3; // 0.3%
+    uint256 public constant FEE_DENOMINATOR = 1000;
+    uint256 public constant FEE_NUMERATOR = 997;
 
     error InvalidAmount();
+    error InvalidToken();
     error InsufficientLiquidity();
+    error SlippageExceeded();
 
     event LiquidityAdded(address indexed user, uint256 amount0, uint256 amount1, uint256 liquidity);
+
+    event LiquidityRemoved(address indexed user, uint256 amount0, uint256 amount1, uint256 liquidity);
+
+    event Swapped(address indexed user, address indexed tokenIn, uint256 amountIn, uint256 amountOut);
 
     constructor(address _token0, address _token1) {
         token0 = IERC20(_token0);
@@ -31,15 +39,32 @@ contract AMM {
         lpToken = new LPToken();
     }
 
-    function addLiquidity(uint256 amount0, uint256 amount1) external returns (uint256 liquidity) {
+    function addLiquidity(uint256 amount0, uint256 amount1, uint256 minLiquidity)
+        external
+        nonReentrant
+        returns (uint256 liquidity)
+    {
         if (amount0 == 0 || amount1 == 0) {
             revert InvalidAmount();
         }
 
         token0.safeTransferFrom(msg.sender, address(this), amount0);
+
         token1.safeTransferFrom(msg.sender, address(this), amount1);
 
-        liquidity = amount0 + amount1;
+        if (lpToken.totalSupply() == 0) {
+            liquidity = sqrt(amount0 * amount1);
+        } else {
+            liquidity = min((amount0 * lpToken.totalSupply()) / reserve0, (amount1 * lpToken.totalSupply()) / reserve1);
+        }
+
+        if (liquidity < minLiquidity) {
+            revert SlippageExceeded();
+        }
+
+        if (liquidity == 0) {
+            revert InsufficientLiquidity();
+        }
 
         lpToken.mint(msg.sender, liquidity);
 
@@ -49,7 +74,11 @@ contract AMM {
         emit LiquidityAdded(msg.sender, amount0, amount1, liquidity);
     }
 
-    function removeLiquidity(uint256 liquidity) external returns (uint256 amount0, uint256 amount1) {
+    function removeLiquidity(uint256 liquidity, uint256 minAmount0, uint256 minAmount1)
+        external
+        nonReentrant
+        returns (uint256 amount0, uint256 amount1)
+    {
         if (liquidity == 0) {
             revert InvalidAmount();
         }
@@ -57,10 +86,11 @@ contract AMM {
         uint256 totalSupply = lpToken.totalSupply();
 
         amount0 = (liquidity * reserve0) / totalSupply;
+
         amount1 = (liquidity * reserve1) / totalSupply;
 
-        if (amount0 == 0 || amount1 == 0) {
-            revert InsufficientLiquidity();
+        if (amount0 < minAmount0 || amount1 < minAmount1) {
+            revert SlippageExceeded();
         }
 
         lpToken.burn(msg.sender, liquidity);
@@ -70,6 +100,8 @@ contract AMM {
 
         token0.safeTransfer(msg.sender, amount0);
         token1.safeTransfer(msg.sender, amount1);
+
+        emit LiquidityRemoved(msg.sender, amount0, amount1, liquidity);
     }
 
     function getAmountOut(uint256 amountIn, uint256 reserveIn, uint256 reserveOut)
@@ -77,21 +109,28 @@ contract AMM {
         pure
         returns (uint256 amountOut)
     {
-        if (amountIn == 0) revert InvalidAmount();
+        if (amountIn == 0) {
+            revert InvalidAmount();
+        }
+
         if (reserveIn == 0 || reserveOut == 0) {
             revert InsufficientLiquidity();
         }
 
-        uint256 amountInWithFee = amountIn * 997;
+        uint256 amountInWithFee = amountIn * FEE_NUMERATOR;
 
         uint256 numerator = amountInWithFee * reserveOut;
 
-        uint256 denominator = (reserveIn * 1000) + amountInWithFee;
+        uint256 denominator = (reserveIn * FEE_DENOMINATOR) + amountInWithFee;
 
         amountOut = numerator / denominator;
     }
 
-    function swap(address tokenIn, uint256 amountIn) external returns (uint256 amountOut) {
+    function swap(address tokenIn, uint256 amountIn, uint256 minAmountOut)
+        external
+        nonReentrant
+        returns (uint256 amountOut)
+    {
         if (amountIn == 0) {
             revert InvalidAmount();
         }
@@ -99,7 +138,7 @@ contract AMM {
         bool isToken0 = tokenIn == address(token0);
 
         if (tokenIn != address(token0) && tokenIn != address(token1)) {
-            revert InvalidAmount();
+            revert InvalidToken();
         }
 
         (IERC20 inputToken, IERC20 outputToken, uint256 reserveIn, uint256 reserveOut) =
@@ -109,6 +148,10 @@ contract AMM {
 
         amountOut = getAmountOut(amountIn, reserveIn, reserveOut);
 
+        if (amountOut < minAmountOut) {
+            revert SlippageExceeded();
+        }
+
         outputToken.safeTransfer(msg.sender, amountOut);
 
         if (isToken0) {
@@ -117,6 +160,26 @@ contract AMM {
         } else {
             reserve1 += amountIn;
             reserve0 -= amountOut;
+        }
+
+        emit Swapped(msg.sender, tokenIn, amountIn, amountOut);
+    }
+
+    function min(uint256 x, uint256 y) internal pure returns (uint256) {
+        return x < y ? x : y;
+    }
+
+    function sqrt(uint256 y) internal pure returns (uint256 z) {
+        if (y > 3) {
+            z = y;
+            uint256 x = y / 2 + 1;
+
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
         }
     }
 }
